30790d564b604c719053875053016178942770a2
VitolLub <VitolLub@gmail.com>
2021-09-15 20:03:41 +0300

    Delete Elogic/TestUnit directory

--- Elogic/TestUnit/Block/Product/Listing/Brand.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Elogic\TestUnit\Block\Product\Listing;
-
-class Brand extends \Magento\Backend\Block\Template
-{
-
-}
--- Elogic/TestUnit/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Elogic_TestUnit',
-    __DIR__
-);
